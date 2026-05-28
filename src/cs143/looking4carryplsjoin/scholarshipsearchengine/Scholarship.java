e6df02f314e41cfadea86151bcf1db82b28f5c92
dxvu4-dev <dxvu4@students.everettcc.edu>
2026-05-28 12:12:47 -0700

    Add files via upload

--- src/cs143/looking4carryplsjoin/scholarshipsearchengine/Scholarship.java
+++ src/cs143/looking4carryplsjoin/scholarshipsearchengine/Scholarship.java
@@ -1,5 +1,29 @@
 package cs143.looking4carryplsjoin.scholarshipsearchengine;
 
-public class Scholarship {
-
+public abstract class Scholarship {
+	//private fields
+	private double GPA;
+	private String race;
+	private String gender;
+	private double income;
+	private String major;
+	private boolean isFirstGenImmigrant;
+	private boolean isWashingtonResident;
+	//default constructor
+	Scholarship() {
+		this(0.0, "White", "Male", 0.0, "Business", false, false);
+	}
+	//parameterized constructor
+	Scholarship(double GPA, String race, String gender, double income, String major, boolean isFirstGenImmigrant, boolean isWashingtonResident) {
+		this.GPA = GPA;
+		this.race = race;
+		this.gender = gender;
+		this.income = income;
+		this.major = major;
+		this.isFirstGenImmigrant = isFirstGenImmigrant;
+		this.isWashingtonResident = isWashingtonResident;
+	}
+	//setter methods
+		
+	//getter methods
 }
